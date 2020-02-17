c172f51b160c58c76509e1d5c2cfb33f24f808c5
Marcodalmas00 <60871985+Marcodalmas00@users.noreply.github.com>
2020-02-17 12:24:26 +0100

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,6 +1,14 @@
 <?php
-
+	
+	include_once 'dbConnection.php';
     session_start();
+
+    $id = $_SESSION['id'];
+
+    $stmt = $pdo->prepare('DELETE FROM activity 
+		  				   WHERE uid = ?');
+
+    $stmt->execute([$id]);
 
     $_SESSION = [];
 
